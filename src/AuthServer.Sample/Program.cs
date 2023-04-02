56dcea6c7b137b260c7db93046d0b101cfd3c286
hasmukhlalpatel <hasmukhlalpatel@live.co.uk>
2023-04-02 21:15:36 +0100

    refactoed /.well-known/openid-configuration

--- src/AuthServer.Sample/Program.cs
+++ src/AuthServer.Sample/Program.cs
@@ -1,37 +1,26 @@
+using AuthServer.Sample.Services;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services
+    .AddSingleton<ResourceReader>()
+    .AddTransient<WellKnownOpenidConfiguration>();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/.well-known/openid-configuration", () =>
+app.MapGet(WellKnownOpenidConfiguration.ConstV1Url, (WellKnownOpenidConfiguration configuration, HttpRequest request) =>
 {
-    var assembly = Assembly.GetExecutingAssembly();
-    var resourceName = "AuthServer.Sample.Resources.openid-configuration.json";
-
-    //var rr = assembly.GetManifestResourceNames();
-
-    using (Stream stream = assembly.GetManifestResourceStream(resourceName))
-    using (StreamReader reader = new StreamReader(stream))
-    {
-        return reader.ReadToEnd();
-    }
+    var siteName = $"{request.Scheme}//{request.Host.ToUriComponent()}";
+    var appId = string.Empty;
+    return Results.Text(configuration.GetV1(siteName, appId), "application/json"); 
 });
 
-app.MapGet("/v2.0/.well-known/openid-configuration", () =>
+app.MapGet(WellKnownOpenidConfiguration.ConstV2Url, (WellKnownOpenidConfiguration configuration, HttpRequest request) =>
 {
-    var assembly = Assembly.GetExecutingAssembly();
-    var resourceName = "AuthServer.Sample.Resources.V2.openid-configuration.json";
-
-    //var rr = assembly.GetManifestResourceNames();
-
-    using (Stream stream = assembly.GetManifestResourceStream(resourceName))
-    using (StreamReader reader = new StreamReader(stream))
-    {
-        return reader.ReadToEnd();
-    }
+    var siteName = $"{request.Scheme}//{request.Host.ToUriComponent()}";
+    var appId = string.Empty;
+    return Results.Text(configuration.GetV2(siteName, appId), "application/json");
 });
-
 
 app.Run();
---
+++ src/AuthServer.Sample/Services/ResourceReader.cs
@@ -0,0 +1,50 @@
+﻿using System.Reflection;
+
+namespace AuthServer.Sample.Services;
+
+public class ResourceReader
+{
+    private readonly Assembly _assembly;
+    public ResourceReader()
+    {
+        _assembly = Assembly.GetExecutingAssembly();
+    }
+
+    public string GetStringFromResource(string resourceName)
+    {
+        using Stream stream = _assembly.GetManifestResourceStream(resourceName);
+        using StreamReader reader = new StreamReader(stream);
+        return reader.ReadToEnd();
+    }
+}
+
+public class WellKnownOpenidConfiguration
+{
+    //"/.well-known/openid-configuration"
+    public const string ConstV1Url = "/.well-known/openid-configuration";
+    public const string ConstV2Url = "/v2.0/.well-known/openid-configuration";
+    private const string ConstV1ConfigresourceName = "AuthServer.Sample.Resources.openid-configuration.json";
+    private const string ConstV2ConfigresourceName = "AuthServer.Sample.Resources.V2.openid-configuration.json";
+
+    private readonly ResourceReader _resourceReader;
+
+    public WellKnownOpenidConfiguration(ResourceReader resourceReader)
+    {
+        _resourceReader = resourceReader;
+    }
+
+    public string GetV1(string siteName, string appId)
+    {
+        return _resourceReader.GetStringFromResource(ConstV1ConfigresourceName)
+            .Replace("__AUTH_ENDPOINT__", siteName)
+            .Replace("__AUTH_UID__", appId);
+    }
+
+    public string GetV2(string siteName, string appId)
+    {
+        return _resourceReader.GetStringFromResource(ConstV2ConfigresourceName)
+            .Replace("__AUTH_ENDPOINT__", siteName)
+            .Replace("__AUTH_UID__", appId);
+    }
+
+}
