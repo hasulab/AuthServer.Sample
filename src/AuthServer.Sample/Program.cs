ab93cd461c50d72ea920ec9f58c069462eb8e747
hasmukhlalpatel <hasmukhlalpatel@live.co.uk>
2023-04-03 21:30:35 +0100

    added tenantId suport

--- src/AuthServer.Sample/Program.cs
+++ src/AuthServer.Sample/Program.cs
@@ -1,26 +1,54 @@
 using AuthServer.Sample.Services;
+using Microsoft.Extensions.Configuration;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services
     .AddSingleton<ResourceReader>()
-    .AddTransient<WellKnownOpenidConfiguration>();
+    .AddTransient<WellKnownOpenidConfiguration>()
+    .AddTransient<OAuth2Token>();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
-app.MapGet(WellKnownOpenidConfiguration.ConstV1Url, (WellKnownOpenidConfiguration configuration, HttpRequest request) =>
+app.Use(async (context, next) =>
 {
-    var siteName = $"{request.Scheme}//{request.Host.ToUriComponent()}";
-    var appId = string.Empty;
-    return Results.Text(configuration.GetV1(siteName, appId), "application/json"); 
+    Console.WriteLine($"1. Endpoint: {context.GetEndpoint()?.DisplayName ?? "(null)"}");
+    Console.WriteLine(context.Request.ContentType);
+    await next(context);
 });
 
-app.MapGet(WellKnownOpenidConfiguration.ConstV2Url, (WellKnownOpenidConfiguration configuration, HttpRequest request) =>
+
+app.MapGet("/", (LinkGenerator linker) =>
+       {
+           var v1Url = linker.GetPathByName("v1-well-known-config", values: new { tenantId = Guid.Empty });
+           var v2Url = linker.GetPathByName("v2-well-known-config", values: new { tenantId = Guid.Empty });
+           var htmlBody = $"The link to the /.well-known/openid-configuration route of <a href='{v1Url}'>v1</a>  <a href='{v2Url}'>v2</a> ";
+           return Results.Content($"<html><body></body>{htmlBody}</html>", "text/html; charset=utf-8");
+       });
+
+app.MapGet(WellKnownOpenidConfiguration.ConstV1Url, (WellKnownOpenidConfiguration configuration, HttpRequest request, string tenantId) =>
 {
     var siteName = $"{request.Scheme}//{request.Host.ToUriComponent()}";
-    var appId = string.Empty;
-    return Results.Text(configuration.GetV2(siteName, appId), "application/json");
+    return Results.Text(configuration.GetV1(siteName, tenantId), "application/json"); 
+})
+    .WithName("v1-well-known-config");
+
+app.MapGet(WellKnownOpenidConfiguration.ConstV2Url, (WellKnownOpenidConfiguration configuration, HttpRequest request, string tenantId) =>
+{
+    var siteName = $"{request.Scheme}//{request.Host.ToUriComponent()}";
+    return Results.Text(configuration.GetV2(siteName, tenantId), "application/json");
+})
+    .WithName("v2-well-known-config");
+
+app.MapPost(OAuth2Token.ConstV1Url, (OAuth2Token tokenService, OAuthTokenRequest tokenRequest) =>
+{
+    return Results.Text(tokenService.GetResponse(tokenRequest), "application/json");
+});
+
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"1. Endpoint: {context.GetEndpoint()?.DisplayName ?? "(null)"}");
+    Console.WriteLine(context.Request.ContentType);
+    await next(context);
 });
 
 app.Run();
--- src/AuthServer.Sample/Services/ResourceReader.cs
+++ src/AuthServer.Sample/Services/ResourceReader.cs
@@ -21,8 +21,8 @@ public class ResourceReader
 public class WellKnownOpenidConfiguration
 {
     //"/.well-known/openid-configuration"
-    public const string ConstV1Url = "/.well-known/openid-configuration";
-    public const string ConstV2Url = "/v2.0/.well-known/openid-configuration";
+    public const string ConstV1Url = "/{tenantId}/.well-known/openid-configuration";
+    public const string ConstV2Url = "/{tenantId}/v2.0/.well-known/openid-configuration";
     private const string ConstV1ConfigresourceName = "AuthServer.Sample.Resources.openid-configuration.json";
     private const string ConstV2ConfigresourceName = "AuthServer.Sample.Resources.V2.openid-configuration.json";
 
@@ -33,18 +33,73 @@ public class WellKnownOpenidConfiguration
         _resourceReader = resourceReader;
     }
 
-    public string GetV1(string siteName, string appId)
+    public string GetV1(string endpoint, string tenantId)
     {
-        return _resourceReader.GetStringFromResource(ConstV1ConfigresourceName)
-            .Replace("__AUTH_ENDPOINT__", siteName)
-            .Replace("__AUTH_UID__", appId);
+        return GetResourceText(ConstV1ConfigresourceName, endpoint, tenantId);
     }
 
-    public string GetV2(string siteName, string appId)
+    public string GetV2(string endpoint, string tenantId)
     {
-        return _resourceReader.GetStringFromResource(ConstV2ConfigresourceName)
-            .Replace("__AUTH_ENDPOINT__", siteName)
-            .Replace("__AUTH_UID__", appId);
+        return GetResourceText(ConstV2ConfigresourceName, endpoint, tenantId);
     }
 
+    private string GetResourceText(string resourceName, string endpoint, string tenantId)
+    {
+        if (string.IsNullOrEmpty(resourceName))
+        {
+            throw new ArgumentException($"'{nameof(resourceName)}' cannot be null or empty.", nameof(resourceName));
+        }
+
+        if (string.IsNullOrEmpty(endpoint))
+        {
+            throw new ArgumentException($"'{nameof(endpoint)}' cannot be null or empty.", nameof(endpoint));
+        }
+
+        if (string.IsNullOrEmpty(tenantId))
+        {
+            throw new ArgumentException($"'{nameof(tenantId)}' cannot be null or empty.", nameof(tenantId));
+        }
+        return _resourceReader.GetStringFromResource(resourceName)
+            .Replace("__AUTH_ENDPOINT__", endpoint)
+            .Replace("__AUTH_UID__", tenantId);
+    }
+}
+
+public class OAuth2Token
+{
+    //"/oauth2/token"
+    public const string ConstV1Url = "/{tenantId}/oauth2/token";
+    public const string ConstV2Url = "/{tenantId}/oauth2/v2.0/token";
+    //oauth-token-access_token-response
+    private const string ConstV1ConfigresourceName = "AuthServer.Sample.Resources.oauth-token-access_token-response.json";
+    private const string ConstV2ConfigresourceName = "AuthServer.Sample.Resources.V2.openid-configuration.json";
+
+    private readonly ResourceReader _resourceReader;
+
+    public OAuth2Token(ResourceReader resourceReader)
+    {
+        _resourceReader = resourceReader;
+    }
+
+    public string GetResponse(OAuthTokenRequest tokenRequest)
+    {
+        return GetResourceText(ConstV1ConfigresourceName, "", "");
+    }
+    private string GetResourceText(string resourceName, string endpoint, string tenantId)
+    {
+        return _resourceReader.GetStringFromResource(resourceName);
+    }
+}
+public class OAuthTokenRequest
+{
+    public string grant_type { get; set; } = "client_credentials";
+     public string client_id { get; set; }
+    public string client_secret { get; set; }
+}
+
+public class OAuth2Authorize
+{
+    //"/oauth2/authorize"
+    public const string ConstV1Url = "/{tenantId}/oauth2/authorize";
+    public const string ConstV2Url = "/{tenantId}/oauth2/v2.0/authorize";
 }
